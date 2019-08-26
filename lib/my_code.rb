1ee5c7b212c96e3e4f434e51d95f162eb200f8b3
Jinming Zhang <jinming.zhang.92@gmail.com>
2019-08-26 00:31:21 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,22 @@
 # Your Code Here
+def map(array)
+  new_array=[]
+  array.length.times {|index|
+    new_array[index]=yield(array[index])
+  }
+  new_array
+end
+
+def reduce(array,starting_point=nil)
+  if starting_point
+    total=starting_point
+    index=0
+  else
+    total=array[0]
+    index=1
+  end
+  while index<array.length do 
+    total=yield(total,array[index])
+    total
+  end
+  end
