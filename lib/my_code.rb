ecf132c8d49b6c14c25f32d2ebb5ee767923615f
Viachaslau Pashaliuk <v.pashaliuk@gmail.com>
2019-09-10 05:17:55 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,17 @@
 # Your Code Here
+def map(source_array)
+  new_array = []
+  source_array.each{|num| new_array << yield(num)}
+  new_array
+end
+
+def reduce(source_array, starting_point = nil)
+  if starting_point
+    total = starting_point
+    source_array.each{|element| total = yield(total, element)}
+  else
+    total = source_array[0]
+    source_array.drop(1).each{|element| total = yield(total, element)}
+  end
+  total
+end
